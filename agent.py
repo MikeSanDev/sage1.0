249eae7276479fce4092e888a82794451c882c58
Michael <mjs070594@gmail.com>
2025-07-08 10:46:22 -0500

    livekit session attempt

--- agent.py
+++ agent.py
@@ -13,18 +13,18 @@ load_dotenv()
 #class that takes care of our agent which is OOB of livekit
 class Assistant(Agent):
     def __init__(self) -> None:
-        super().__init__(instructions=AGENT_INSTRUCTION,
-            llm=google.beta.realtime.RealtimeModel(
-            voice="Aoede" 
-            # You can use any of the available voices in the RealtimeModel
-            # For a list of available voices, see:  # https://cloud.google.com/vertex-ai/docs/generative-ai/voice/voices 
-        ))
+        super().__init__(instructions=AGENT_INSTRUCTION)
 
 #most important part of the code, this is where we start our agent session
 #this is where we connect to our room and start the agent session
 async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
-
+        llm=google.beta.realtime.RealtimeModel(
+            voice="Aoede",
+            temperature=0.7,
+            # You can use any of the available voices in the RealtimeModel
+            # For a list of available voices, see:  # https://cloud.google.com/vertex-ai/docs/generative-ai/voice/voices 
+        )
     )
 #start the session with the room and agent we created above
     await session.start(
@@ -51,5 +51,6 @@ if __name__ == "__main__":
     agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
 
     # use python ./agent.py console to run the worker in console mode
+    # use python .\agent.py dev to run the worker in dev mode (with auto-reload on file changes)
 
     #voices # - Aoede (Breezy), Zephyr (Bright) 
